f2da8b1c19e46c592b7df3f55414a09db4a5704e
Jean-Patrick Bernhardt <jp.bernhardt@gmail.com>
2021-03-16 18:29:54 +0100

    updated port config2

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ app.put("/image", (req, res) => { image.handleImage(req, res, db) });
 app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on Port: " + process.env.PORT || "3000");
+  console.log("Server is running on Port: " + process.env.PORT);
 });
