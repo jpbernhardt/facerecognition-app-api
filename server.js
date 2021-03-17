d6470d4187dbeaf6a7d0d4a12dca3c5383c00d87
Jean-Patrick Bernhardt <jp.bernhardt@gmail.com>
2021-03-17 14:42:32 +0100

    fix

--- server.js
+++ server.js
@@ -12,15 +12,9 @@ const image = require("./controllers/image");
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
-    ssl: true
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
-  // connection: {
-  //   host : '127.0.0.1',
-  //   user : 'jpbernhardt',
-  //   password : '',
-  //   database : process.env.DATABASE_URL
-  // }
 });
 
 const app = express();
@@ -33,20 +27,6 @@ function getUserId(req) {
 }
 
 app.get("/", (req, res) => { res.send('db.database.user') });
-app.get('/db', async (req, res) => {
-  try {
-    const client = await db.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    console.log(result);
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-  }
-})
-
 app.post("/signin", (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) });
